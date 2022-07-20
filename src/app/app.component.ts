24734c5d30d8b1e2da059156072c41017bc0590b
Jan Razny <netorg@gmx.de>
2022-07-20 12:51:04 +0200

    slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'janrazny';
-  
+export class AppComponent implements OnInit {
+  images = ['development0.jpg','development1.jpg','development2.jpg','development3.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
 
 
 }

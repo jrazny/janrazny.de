adcbe785d4730cef49b3c7d5f636a8c018e107d5
Jan Razny <netorg@gmx.de>
2022-07-21 13:30:01 +0200

    build 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ImpressumComponent } from './impressum/impressum.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 const routes: Routes = [
   { path: '', component: SlideshowComponent },
-  { path: 'impressum', component: ImpressumComponent }
+  { path: 'impressum', component: ImpressumComponent },
+  { path: 'data-protection', component: DataProtectionComponent }
 ];
 
 @NgModule({

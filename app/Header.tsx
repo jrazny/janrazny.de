08213ada1a87d78bd1f1053e7901e22ab4935668
Jan Razny <netorg@gmx.de>
2023-03-30 00:02:49 +0200

    socialIcons

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,18 +1,53 @@
 import Link from "next/link";
 import { MdAddHome } from "react-icons/md";
+import { SocialIcon } from 'react-social-icons';
 
 const Header = () => {
   return (
     <>
       <header className="z-20 flex items-center justify-between px-5 py-3 shadow-lg bg-violet-100 text-zinc-800 dark:bg-zinc-800 dark:text-white h-[60px] sticky top-0">
         <Link href="/" className="flex items-center space-x-1 text-lg font-light tracking-widest">
-            <span>
-              CMP
+            <span className="text-sky-700">
+              JR
             </span>
             <i>
-              <MdAddHome className="w-10 h-10" />
+              <MdAddHome className="w-10 h-10 text-sky-700" />
             </i>
         </Link>
+        <div className="hidden space-x-5 sm:items-center sm:flex">
+          <a href="#technologies" className="text-lg font-bold text-sky-700">
+            Technologien
+          </a>
+          <a href="#about" className="text-lg font-bold text-sky-700">
+            Über ich
+          </a>
+          <a href="#projects" className="text-lg font-bold text-sky-700">
+            Projekte
+          </a>
+          <a href="#contact" className="text-lg font-bold text-sky-700">
+            Kontakt
+          </a>
+        </div>
+        <div className="flex items-center space-x-5">
+          <SocialIcon
+            url="https://www.instagram.com/#" 
+            bgColor="rgb(3,105,161)" 
+            fgColor="white"
+            style={{width:40,height:40}} 
+          />
+          <SocialIcon
+            url="https://github.com/jrazny" 
+            bgColor="rgb(3,105,161)"
+            fgColor="white" 
+            style={{width:40,height:40}} 
+          />
+          <SocialIcon
+            url="https://www.facebook.com/#" 
+            bgColor="rgb(3,105,161)" 
+            fgColor="white" 
+            style={{width:40,height:40}} 
+          />          
+        </div>
       </header>
     </>
   );
